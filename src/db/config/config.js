a9b7fde9a76e34247a15b774b4c27b30da758e97
omobosteven <omobosteven@gmail.com>
2019-10-18 21:41:08 +0100

    setup express and sequelize

---
+++ src/db/config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
+  test: {
+    use_env_variable: 'TEST_DATABASE_URL',
+    dialect: 'postgres',
+    logging: false
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];
---
+++ src/db/models/comment.js
@@ -0,0 +1,22 @@
+const comment = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('comment', {
+    episode_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    public_ip: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    comment: {
+      type: DataTypes.STRING(500),
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+  });
+
+  return Comment;
+};
+
+export default comment;
