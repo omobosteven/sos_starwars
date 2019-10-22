431054c034f078e8adbeea0dd6e537b6e9d29fe8
omobosteven <omobosteven@gmail.com>
2019-10-22 23:34:21 +0100

    fix regenerator error on heroku

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import 'dotenv/config';
-import 'regenerator-runtime/runtime';
 import express from 'express';
 import router from './routes';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => res.status(200).send({
+    status: 200,
+    message: 'sos starwars movies!!!'
+}));
+
 app.use(router);
 
-app.listen(3000, () => console.log(`listening on port ${process.env.PORT}!`),);
+app.listen(port, () => console.log(`listening on port ${port}!`),);
